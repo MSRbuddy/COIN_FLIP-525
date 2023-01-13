0ed31371e7374a0a422afc317ea7a0a122c78081
MSRbuddy <meghanadasari222@gmail.com>
2023-01-13 20:25:02 +0530

    Added: Flipping a Coin Multiple Times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -10,3 +10,27 @@ else
 	echo "TAILS";
 fi
 
+#UC-2 -> Flipping a Coin Multiple times
+
+HEADS=1;
+TAILS=0;
+count1=0;
+count2=0;
+Flip=$((RANDOM%2))
+if [ $Flip -eq $HEADS ]
+then
+	echo "HEADS";
+	while [ $count1 -le 21 ]
+	do
+		echo "$count1";
+		count1=$(($count1+1));
+	done
+else
+	echo "TAILS";
+	while [ $count2 -le 21 ]
+	do
+		echo "$count2";
+		count2=$(($count2+1));
+	done
+fi
+
